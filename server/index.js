922595ec181f44ad816d8f70a707cbf434a02c80
Marcus <marcus.yks27@gmail.com>
2025-05-06 01:52:16 +0800

    tried deployment

--- server/index.js
+++ server/index.js
@@ -24,11 +24,13 @@ const io = new Server(httpServer, {
   }
 });
 
-app.use(express.static(path.join(__dirname, '../build')));
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  });
+}
 
 const dataStorage = new DataStorage();
 const roomManager = new RoomManager(dataStorage);
