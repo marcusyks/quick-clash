167b1278bab5e47e2223d5a4be8d2ba6f3f99346
Marcus <marcus.yks27@gmail.com>
2025-05-06 02:03:00 +0800

    found bug

--- server/index.js
+++ server/index.js
@@ -24,10 +24,10 @@ const io = new Server(httpServer, {
   }
 });
 
-if (process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '../build')));
 
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../build', 'index.html'));
   });
 }
